45b60cc7ecfa0ca912a4a58b4bb41d7575679636
Matus Kmit <matus.kmit@zhdk.ch>
2015-07-13 17:31:05 +0200

    io factories

---
+++ spec/factories/io_interfaces.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+
+  factory :io_interface do
+    id { Faker::Lorem.characters(10) }
+  end
+
+end
---
+++ spec/factories/io_mappings.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+
+  factory :io_mapping do
+    io_interface { IoInterface.first || create(:io_interface) }
+    key_map { Faker::Lorem.word }
+    meta_key
+  end
+
+end
