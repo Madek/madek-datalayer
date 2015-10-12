7e43e3e2f21f5af6eacb6f23fc9278774faae236
Matus Kmit <matus.kmit@zhdk.ch>
2015-10-12 17:05:27 +0200

    extend keywords filter

--- app/models/concerns/keywords/filters.rb
+++ app/models/concerns/keywords/filters.rb
@@ -4,15 +4,20 @@ module Concerns
       extend ActiveSupport::Concern
 
       included do
-        scope :filter_by, lambda { |term|
+        scope :filter_by, lambda { |term, meta_key_id|
           if UUIDTools::UUID_REGEXP =~ term
             where(id: term)
           else
-            joins(:meta_key)
-              .where(
-                'keywords.term ILIKE :t OR meta_keys.id ILIKE :t',
-                t: "%#{term}%"
-              )
+            keywords = all
+            if meta_key_id
+              keywords = \
+                keywords.joins(:meta_key).where(meta_key_id: meta_key_id)
+            end
+            if term
+              keywords = \
+                keywords.where('keywords.term ILIKE :t', t: "%#{term}%")
+            end
+            keywords
           end
         }
       end
