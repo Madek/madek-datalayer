2228155efc82f0a4e476aed637c1ce00b40c49a3
Thomas Schank <DrTom@schank.ch>
2015-12-22 10:54:14 +0100

    Add max_duration_secs option to build_session_value

--- lib/madek_open_session.rb
+++ lib/madek_open_session.rb
@@ -6,10 +6,11 @@ module MadekOpenSession
 
   extend ActiveSupport::Concern
 
-  def build_session_value(user)
+  def build_session_value(user,
+        max_duration_secs: Madek::Constants::MADEK_SESSION_VALIDITY_DURATION)
     CiderCi::OpenSession::Encryptor.encrypt(
       secret, user_id: user.id, signature: create_user_signature(user),
-              issued_at: Time.now.iso8601)
+              issued_at: Time.now.iso8601, max_duration_secs: max_duration_secs)
   end
 
   def validate_not_expired!(session_object)
@@ -17,6 +18,11 @@ module MadekOpenSession
       raise(StandardError, 'Session issued_at could not be determined!')
     if issued_at + Madek::Constants::MADEK_SESSION_VALIDITY_DURATION < Time.now
       raise(StandardError, 'Session is expired!')
+    end
+    if session_object[:max_duration_secs]
+      if issued_at + session_object[:max_duration_secs] < Time.now
+        raise(StandardError, 'Session is expired!')
+      end
     end
   end
 
