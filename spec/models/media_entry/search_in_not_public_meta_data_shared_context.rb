0b99405e944fe017dae13e4d951cc7d6a51b3268
Matúš Kmiť <matus.kmit@zhdk.ch>
2016-04-06 13:10:49 +0200

    shared context

---
+++ spec/models/media_entry/search_in_not_public_meta_data_shared_context.rb
@@ -0,0 +1,23 @@
+RSpec.shared_context 'meta data from not public vocabulary shared context' do
+  let(:vocabulary) { FactoryGirl.create(:vocabulary, id: 'filter') }
+  let(:responsible_user) { FactoryGirl.create(:user) }
+  let(:meta_key) do
+    FactoryGirl.create(:meta_key_text,
+                       id: "#{vocabulary.id}:#{Faker::Lorem.characters(20)}",
+                       vocabulary: vocabulary)
+  end
+  let (:media_entry) do
+    media_entry = FactoryGirl.create(:media_entry_with_image_media_file,
+                                     responsible_user: responsible_user)
+    media_entry
+  end
+
+  # META DATA  ##########################################################
+  let(:meta_datum_text) do
+    meta_datum_text = FactoryGirl.create(:meta_datum_text,
+                                         meta_key: meta_key,
+                                         value: 'a par tial match')
+    media_entry.meta_data << meta_datum_text
+    meta_datum_text
+  end
+end
