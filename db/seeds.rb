acaa332e8023b420dcc005e6e3140c8b91fdfd36
Thomas Schank <DrTom@schank.ch>
2016-06-22 18:32:42 +0200

    Set resp seed core vocabulary, meta_keys, context and context_keys

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+  ActiveRecord::Base.connection.execute <<-SQL.strip_heredoc
+    SET session_replication_role = replica;
+  SQL
+
+  Vocabulary.find_or_create_by(id: 'madek_core').update_attributes!  \
+    label: 'Madek Core',
+    description: 'This is the predefined and immutable Madek core vocabulary.'
+
+  Context.find_or_create_by(id: 'core').update_attributes! \
+    label: "Core",
+    description: "Die Metadaten dieses Kontextes sind in der ME-Detailansicht links neben dem Thumbnail sichtbar. Der Core-Kontext wird auch in der Listenansicht usw. verwendet."
+
+  YAML.load_file(Rails.root.join("db","madek_core_meta_keys.yml")).each do |id,attrs|
+    MetaKey.find_or_initialize_by(id: id).update_attributes! attrs
+    ContextKey.find_or_create_by(context_id: 'core', meta_key_id: id)
+  end
+
+  %w(madek_core:title madek_core:copyright_notice).each do |mkid|
+    ContextKey.find_by(meta_key_id: mkid, context_id: 'core') \
+      .update_attributes! is_required: true
+  end
+
+  ActiveRecord::Base.connection.execute <<-SQL.strip_heredoc
+    SET session_replication_role = DEFAULT;
+  SQL
+end
