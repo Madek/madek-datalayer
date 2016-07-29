cb20cd672a2b2951888e14259fd747c1898e3d46
Michał Podlecki <m.podlecki@op.pl>
2016-07-29 09:49:39 +0200

    update api client factory with password attribute

--- spec/factories/api_clients.rb
+++ spec/factories/api_clients.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :api_client do
     login { Faker::Lorem.words(5).shuffle.join('_').slice(0, 20) }
     description { Faker::Lorem.words(10).join(' ') }
+    password 'securepassword'
     user { User.find_random || (FactoryGirl.create :user) }
   end
 end
