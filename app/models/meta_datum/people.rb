48f909b5ee2b225270eb20f6349c353a151b7fc1
Matúš Kmiť <matus.kmit@zhdk.ch>
2016-08-09 13:05:19 +0200

    on the fly person/bunch creation

--- app/models/meta_datum/people.rb
+++ app/models/meta_datum/people.rb
@@ -1,5 +1,10 @@
 class MetaDatum::People < MetaDatum
 
+  ATTRIBUTES_FOR_ON_THE_FLY_RESOURCE_CREATION = [:first_name,
+                                                 :last_name,
+                                                 :pseudonym,
+                                                 :is_bunch]
+
   has_many :meta_data_people,
            class_name: 'MetaDatum::Person',
            foreign_key: :meta_datum_id
@@ -15,6 +20,10 @@ class MetaDatum::People < MetaDatum
 
   def set_value!(people, created_by_user)
     reset_with_sanitized_value!(people, 'person', created_by_user)
+  end
+
+  def sanitize_attributes_for_on_the_fly_resource_creation(attrs_hash)
+    attrs_hash.permit(ATTRIBUTES_FOR_ON_THE_FLY_RESOURCE_CREATION)
   end
 
 end
