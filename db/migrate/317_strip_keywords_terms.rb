d116f8a0493cd5fc19a34c83553e27186b957d59
Matúš Kmiť <matus.kmit@zhdk.ch>
2016-11-14 11:39:24 +0100

    ensure timestamps are preserved

--- db/migrate/317_strip_keywords_terms.rb
+++ db/migrate/317_strip_keywords_terms.rb
@@ -20,8 +20,12 @@ class StripKeywordsTerms < ActiveRecord::Migration
           if other_keyword = ::MigrationKeyword.find_by(meta_key_id: keyword.meta_key_id, term: new_term)
             # change all rows in meta_data_keywords to point keyword_id to the other keyword
             ::MigrationMetaDatumKeyword.where(keyword_id: keyword.id).each do |md|
+              old_created_at = md.created_at
+              old_updated_at = md.updated_at
               # make a copy of the meta_datum
               new_md = md.dup
+              new_md.created_at = old_created_at
+              new_md.updated_at = old_updated_at
               # destroy the meta_datum for the stripped keyword
               md.destroy
               # point keyword_id for the new meta_datum to the other keyword
@@ -29,6 +33,8 @@ class StripKeywordsTerms < ActiveRecord::Migration
               # create new meta_datum_keyword only if there is not one already for the same keyword and meta_datum
               unless ::MigrationMetaDatumKeyword.find_by(keyword_id: new_md.keyword_id, meta_datum_id: new_md.meta_datum_id)
                 new_md.save
+                raise unless new_md.reload.created_at == old_created_at
+                raise unless new_md.reload.updated_at == old_updated_at
               end
             end
 
