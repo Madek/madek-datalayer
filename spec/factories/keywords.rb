44131d12539a7b9be7cd5989f5a7537d2ce91a12
Max Albrecht <max.albrecht@zhdk.ch>
2017-02-27 21:48:22 +0100

    factories: keywords: use simpler terms
    less flacky tests, more funny examples ;)

--- spec/factories/keywords.rb
+++ spec/factories/keywords.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :keyword do
-    term { Faker::Lorem.words.join(' ') }
+    term do
+      [Faker::Hipster.sentence.split(' ').sample,
+       [Faker::Hacker.noun, Faker::Food.ingredient.split(' ').sample]
+      ].flatten.shuffle.join('_').gsub(%r{[.,;\-/\\]}, '').classify
+    end
     meta_key do
       MetaKey.find_by(id: 'test:keywords') \
                || FactoryGirl.create(:meta_key_keywords)
