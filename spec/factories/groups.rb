f5882602e2c9a08b0b28db97051856d90870083b
Michał Podlecki <m.podlecki@op.pl>
2017-04-20 20:03:26 +0200

    factories: groups: add authentication group factory

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -8,18 +8,15 @@ FactoryGirl.define do
         group.users << create(:user)
       end
     end
-  end
 
-  factory :institutional_group do
+    factory :institutional_group do
+      institutional_group_name { Faker::Lorem.words(3).join(' ') }
+      type 'InstitutionalGroup'
+    end
 
-    name { Faker::Lorem.words(3).join(' ') }
-    institutional_group_name { Faker::Lorem.words(3).join(' ') }
-    type { 'InstitutionalGroup' }
-
-    trait :with_user do
-      after(:create) do |group|
-        group.users << create(:user)
-      end
+    factory :authentication_group do
+      type 'AuthenticationGroup'
     end
   end
+
 end
