a5f05b21f351df30b89c3e5873e5c21ae22798cc
phiku <philipp.kuederli@zhdk.ch>
2017-05-09 13:20:16 +0200

    fix: make order by title work in search together with meta data filters

--- app/models/concerns/media_resources/custom_order_by.rb
+++ app/models/concerns/media_resources/custom_order_by.rb
@@ -11,16 +11,25 @@ module Concerns
           when 'created_at DESC'
             reorder('created_at DESC')
           when 'title ASC'
-            joins_meta_data_title
+            joins_meta_data_title_with_projection
               .reorder("meta_data.string ASC, #{table_name}.id ASC")
           when 'title DESC'
-            joins_meta_data_title
+            joins_meta_data_title_with_projection
               .reorder("meta_data.string DESC, #{table_name}.id DESC")
           when 'last_change'
             order_by_last_edit_session.reorder('last_change DESC')
           else
             raise 'Invalid order spec! ' + order_spec
           end
+        end
+
+        private
+
+        def joins_meta_data_title_with_projection
+          joins_meta_data_title
+            .select(
+              all.arel.project('meta_data.string').projections
+            )
         end
       end
     end
