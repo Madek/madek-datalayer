84528710599a629cd6185798e7de082916fce505
phiku <philipp.kuederli@zhdk.ch>
2017-05-22 14:36:11 +0200

    fix: use sub query for ordering for title since meta_data join is sometimes used already for select scope

--- app/models/concerns/media_resources/custom_order_by.rb
+++ app/models/concerns/media_resources/custom_order_by.rb
@@ -4,17 +4,17 @@ module Concerns
       extend ActiveSupport::Concern
 
       module ClassMethods
-        def custom_order_by(order_spec, join_meta_data_for_order: true)
+        def custom_order_by(order_spec)
           case order_spec
           when 'created_at ASC'
             reorder('created_at ASC')
           when 'created_at DESC'
             reorder('created_at DESC')
           when 'title ASC'
-            joins_meta_data_title_with_projection(join_meta_data_for_order)
+            joins_meta_data_title_with_projection
               .reorder("meta_data.string ASC, #{table_name}.id ASC")
           when 'title DESC'
-            joins_meta_data_title_with_projection(join_meta_data_for_order)
+            joins_meta_data_title_with_projection
               .reorder("meta_data.string DESC, #{table_name}.id DESC")
           when 'last_change'
             order_by_last_edit_session.reorder('last_change DESC')
@@ -25,13 +25,17 @@ module Concerns
 
         private
 
-        def joins_meta_data_title_with_projection(join_meta_data_for_order)
-          if join_meta_data_for_order
-            joins_meta_data_title
-          else
-            all
-          end
+        def joins_meta_data_title_with_projection
+          all.unscoped
             .select(all.arel.project('meta_data.string').projections)
+            .joins_meta_data_title
+            .where(
+              <<-SQL
+                #{table_name}.id in (
+                  #{all.select('id').reorder(nil).to_sql}
+                )
+              SQL
+            )
         end
       end
     end
