c2ebf89e405a0022704487dcd617f15a83c5b038
Michał Podlecki <m.podlecki@op.pl>
2017-06-02 14:49:59 +0200

    context_key: add move_to_top/move_to_bottom sorting

--- app/models/context_key.rb
+++ app/models/context_key.rb
@@ -16,4 +16,28 @@ class ContextKey < ActiveRecord::Base
     move :down, context_id: context.id
   end
 
+  def move_to_top
+    ActiveRecord::Base.transaction do
+      regenerate_positions(scope: { context_id: context.id })
+      new_position = 0
+      context.context_keys.where('position < ?', position).each do |sibling|
+        sibling.increment(:position)
+        sibling.save!
+      end
+      update_attribute(:position, new_position)
+    end
+  end
+
+  def move_to_bottom
+    ActiveRecord::Base.transaction do
+      regenerate_positions(scope: { context_id: context.id })
+      new_position = context.context_keys.maximum(:position)
+      context.context_keys.where('position > ?', position).each do |sibling|
+        sibling.decrement(:position)
+        sibling.save!
+      end
+      update_attribute(:position, new_position)
+    end
+  end
+
 end
