1e8b1fa4a01d97c1bdf90cf1ea8ff74429a086e5
Max Albrecht <max.albrecht@zhdk.ch>
2018-03-08 11:26:19 +0100

    feat: prefer exact match when searching users/people by URL/ID/email

--- app/models/concerns/people/filters.rb
+++ app/models/concerns/people/filters.rb
@@ -3,13 +3,6 @@ module Concerns
     module Filters
       extend ActiveSupport::Concern
 
-      included do
-        scope :with_user, -> { joins(:user) }
-        scope :search_by_term, lambda { |term|
-          where('people.searchable ILIKE :t', t: "%#{term}%")
-        }
-      end
-
       # person admin stuff:
       include Concerns::FilterBySearchTerm
 
@@ -18,11 +11,29 @@ module Concerns
           result = where(
             subtype: MetaKey.find(meta_key_id).allowed_people_subtypes
           )
+          exact_match = self.find_exact_matching(term, result) if term
+          return exact_match if exact_match
           if term
             result = result.filter_by_term_using_attributes(term, :searchable)
           end
           result
         end
+      end
+
+      included do
+        scope :with_user, -> { joins(:user) }
+        scope :search_by_term, lambda { |term|
+          where('people.searchable ILIKE :t', t: "%#{term}%")
+        }
+
+        private
+
+        def self.find_exact_matching(term, scope)
+          # NOTE: should only be done for 'UNIQUE' columns!
+          by_id = scope.where(id: term.split('/').last)
+          return by_id if by_id.present?
+        end
+
       end
 
     end
--- app/models/concerns/users/filters.rb
+++ app/models/concerns/users/filters.rb
@@ -6,8 +6,11 @@ module Concerns
 
       included do
         scope :admin_users, -> { joins(:admin) }
+
         scope :filter_by, lambda { |term, search_also_in_person = false|
-          result = all
+          result = where(is_deactivated: false)
+          exact_match = self.find_exact_matching(term, result)
+          return exact_match if exact_match
           search_attributes = ['users.searchable']
           if search_also_in_person
             search_attributes << 'people.searchable'
@@ -15,6 +18,7 @@ module Concerns
           end
           result.filter_by_term_using_attributes(term, *search_attributes)
         }
+
         scope :sort_by, lambda { |attribute|
           case attribute.to_sym
           when :first_name_last_name
@@ -24,6 +28,21 @@ module Concerns
             reorder(attribute)
           end
         }
+
+        private
+
+        def self.find_exact_matching(term, scope)
+          # NOTE: should only be done for 'UNIQUE' columns!
+          by_id = scope.where(id: term.split('/').last)
+          return by_id if by_id.present?
+
+          by_person_id = scope.where(person_id: term.split('/').last)
+          return by_person_id if by_person_id.present?
+
+          by_mail = scope.where(email: term)
+          return by_mail if by_mail.present?
+        end
+
       end
     end
   end
