b7528df5c119e08a8dd9ce14c3b5f1b8512b236c
Matus Kmit <matus.kmit@zhdk.ch>
2019-04-01 12:48:05 +0200

    extended usage count for people

--- app/models/person.rb
+++ app/models/person.rb
@@ -68,17 +68,36 @@ class Person < ActiveRecord::Base
       .reorder('usage_count DESC')
   end
 
+  # rubocop:disable Metrics/MethodLength
   # used in admin
   def self.admin_with_usage_count
-    select('people.*, count(meta_data.id) AS usage_count')
-      .joins(
-        'LEFT OUTER JOIN meta_data_people ON
-         meta_data_people.person_id = people.id'
-      )
-      .joins(
-        'LEFT JOIN meta_data ON
-         meta_data.id = meta_data_people.meta_datum_id'
-      )
+    select(<<-SQL)
+      people.*,
+      COUNT(meta_data_people.meta_datum_id) AS meta_data_usage_count,
+      COUNT(DISTINCT media_entries.id) AS media_entries_usage_count,
+      COUNT(DISTINCT collections.id) AS collections_usage_count
+    SQL
+      .joins(<<-SQL)
+        LEFT JOIN meta_data_people
+        ON meta_data_people.person_id = people.id
+      SQL
+      .joins(<<-SQL)
+        LEFT JOIN meta_data
+        ON meta_data.id = meta_data_people.meta_datum_id
+      SQL
+      .joins(<<-SQL)
+        LEFT JOIN media_entries
+        ON media_entries.id = meta_data.media_entry_id
+      SQL
+      .joins(<<-SQL)
+        LEFT JOIN collections
+        ON collections.id = meta_data.collection_id
+      SQL
+      .where(<<-SQL)
+        (media_entries.id IS NOT NULL AND media_entries.is_published)
+        OR media_entries.id IS NULL
+      SQL
       .group('people.id')
   end
+  # rubocop:enable Metrics/MethodLength
 end
---
+++ spec/models/person_spec.rb
@@ -0,0 +1,99 @@
+require 'spec_helper'
+
+describe Person do
+
+  it 'usage count in meta data' do
+
+    person_1_first_name = Faker::Lorem.characters(8)
+    person_1 = FactoryGirl.create(:person, first_name: person_1_first_name)
+
+    person_2_first_name = Faker::Lorem.characters(8)
+    FactoryGirl.create(:person, first_name: person_2_first_name)
+
+    media_entry = FactoryGirl.create(:media_entry)
+    2.times do
+      FactoryGirl.create(:meta_datum_people,
+                         people: [person_1],
+                         meta_key: FactoryGirl.create(
+                           :meta_key,
+                           meta_datum_object_type: 'MetaDatum::People',
+                           allowed_people_subtypes: ['Person'],
+                           id: "test:#{Faker::Lorem.characters(8)}"
+                         ),
+                         media_entry: media_entry,
+                         collection: nil)
+    end
+    FactoryGirl.create(:meta_datum_people,
+                       people: [person_1],
+                       meta_key: FactoryGirl.create(
+                         :meta_key,
+                         meta_datum_object_type: 'MetaDatum::People',
+                         allowed_people_subtypes: ['Person'],
+                         id: "test:#{Faker::Lorem.characters(8)}"
+                       ),
+                       media_entry: FactoryGirl.create(:media_entry),
+                       collection: nil)
+    # ######################################################################
+    # unpublished entry
+    FactoryGirl.create(:meta_datum_people,
+                       people: [person_1],
+                       meta_key: FactoryGirl.create(
+                         :meta_key,
+                         meta_datum_object_type: 'MetaDatum::People',
+                         allowed_people_subtypes: ['Person'],
+                         id: "test:#{Faker::Lorem.characters(8)}"
+                       ),
+                       media_entry: FactoryGirl.create(:media_entry,
+                                                       is_published: false),
+                       collection: nil)
+    # ######################################################################
+
+    collection = FactoryGirl.create(:collection)
+    2.times do
+      FactoryGirl.create(:meta_datum_people,
+                         people: [person_1],
+                         meta_key: FactoryGirl.create(
+                           :meta_key,
+                           meta_datum_object_type: 'MetaDatum::People',
+                           allowed_people_subtypes: ['Person'],
+                           id: "test:#{Faker::Lorem.characters(8)}"
+                         ),
+                         media_entry: nil,
+                         collection: collection)
+    end
+    FactoryGirl.create(:meta_datum_people,
+                       people: [person_1],
+                       meta_key: FactoryGirl.create(
+                         :meta_key,
+                         meta_datum_object_type: 'MetaDatum::People',
+                         allowed_people_subtypes: ['Person'],
+                         id: "test:#{Faker::Lorem.characters(8)}"
+                       ),
+                       media_entry: nil,
+                       collection: FactoryGirl.create(:collection))
+
+    expect(find_person(person_1_first_name)).to eq \
+      Hash[:meta_data_usage_count, 6,
+           :media_entries_usage_count, 2,
+           :collections_usage_count, 2]
+
+    expect(find_person(person_2_first_name)).to eq \
+      Hash[:meta_data_usage_count, 0,
+           :media_entries_usage_count, 0,
+           :collections_usage_count, 0]
+
+  end
+
+  def find_person(first_name)
+    Person
+      .admin_with_usage_count
+      .where(first_name: first_name)
+      .first
+      .as_json
+      .symbolize_keys
+      .slice(:meta_data_usage_count,
+             :media_entries_usage_count,
+             :collections_usage_count)
+  end
+
+end
