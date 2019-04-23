05745ba5176c4638c301f0faef80ab9bbb72b538
Max Albrecht <max.albrecht@zhdk.ch>
2019-04-23 14:44:01 +0200

    lib: UriAuthorityControl: add ORCID, ResearcherID, Wikidata, IMDb
    + regex fixes for existing providers

--- spec/lib/uri_authority_control_spec.rb
+++ spec/lib/uri_authority_control_spec.rb
@@ -4,7 +4,9 @@ describe UriAuthorityControl do
   describe 'detects correct data' do
     EXAMPLES = [
       { input: 'https://example.com', output: nil },
+
       { input: 'https://another.example.com/viaf/75121530', output: nil },
+
       {
         input: 'https://d-nb.info/gnd/118529579',
         output: {
@@ -17,6 +19,7 @@ describe UriAuthorityControl do
           }
         }
       },
+
       {
         input: 'https://lccn.loc.gov/no97021030',
         output: {
@@ -41,6 +44,7 @@ describe UriAuthorityControl do
           }
         }
       },
+
       {
         input: 'https://viaf.org/viaf/75121530',
         output: {
@@ -52,7 +56,72 @@ describe UriAuthorityControl do
             url: 'https://viaf.org'
           }
         }
+      },
+
+      {
+        input: 'http://www.wikidata.org/entity/Q42',
+        output: {
+          kind: :WIKIDATA,
+          label: 'Q42',
+          provider: {
+            label: 'Wikidata',
+            name: 'Wikidata Entity URI',
+            url: 'https://www.wikidata.org'
+          }
+        }
+      },
+
+      {
+        input: 'https://orcid.org/0000-0002-1825-0097',
+        output: {
+          kind: :ORCID,
+          label: '0000-0002-1825-0097',
+          provider: {
+            label: 'ORCID iD',
+            name: 'Open Researcher and Contributor ID',
+            url: 'https://www.orcid.org'
+          }
+        }
+      },
+
+      {
+        input: 'https://www.imdb.com/name/nm0251868/',
+        output: {
+          kind: :IMDB,
+          label: 'nm0251868',
+          provider: {
+            label: 'IMDb ID',
+            name: 'Internet Movie Database identifier',
+            url: 'https://www.imdb.com/'
+          }
+        }
+      },
+
+      {
+        input: 'https://researcherid.com/rid/K-8011-2013',
+        output: {
+          kind: :ResearcherID,
+          label: 'K-8011-2013',
+          provider: {
+            label: 'ResearcherID',
+            name: 'Web of Science ResearcherID',
+            url: 'https://www.researcherid.com'
+          }
+        }
+      },
+      {
+        input: 'https://www.researcherid.com/rid/K-8011-2013',
+        output: {
+          kind: :ResearcherID,
+          label: 'K-8011-2013',
+          provider: {
+            label: 'ResearcherID',
+            name: 'Web of Science ResearcherID',
+            url: 'https://www.researcherid.com'
+          }
+        }
       }
+
     ]
 
     EXAMPLES.each do |item|
