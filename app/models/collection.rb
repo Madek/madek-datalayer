4562cd8c455f12ab745368495f7129051a3a0c6a
Michał Podlecki <m.podlecki@op.pl>
2019-08-17 02:46:41 +0200

    add scope not_part_of_any_workflow to Collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -76,6 +76,11 @@ class Collection < ApplicationRecord
       .order(:created_at, :id)
   }
 
+  scope :not_part_of_any_workflow, lambda{
+    joins('LEFT JOIN workflows ON workflows.id = collections.workflow_id')
+      .where('workflows.id IS NULL')
+  }
+
   default_scope { where(clipboard_user_id: nil).reorder(:created_at, :id) }
 
   # NOTE: could possibly be made as a DB trigger
