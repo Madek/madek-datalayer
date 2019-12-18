9dac33ec48cc07e6b6e98d7569a4b80aa9a87b78
Michał Podlecki <m.podlecki@op.pl>
2019-12-18 14:07:45 +0100

    lint again

--- app/models/concerns/media_resources/visibility.rb
+++ app/models/concerns/media_resources/visibility.rb
@@ -17,6 +17,7 @@ module Concerns
           where(Hash[self::VIEW_PERMISSION_NAME, true])
         end
 
+        # rubocop:disable Metrics/MethodLength
         def viewable_by_user(user)
           conditions = arel_table[self::VIEW_PERMISSION_NAME].eq(true)
             .or(arel_table[:responsible_user_id].eq(user.id))
@@ -33,9 +34,8 @@ module Concerns
               .or(
                 scope_to_reuse
                   .with_unpublished
-                  .where(
-                    viewable_by_workflow_creator(user)
-                      .or(viewable_by_workflow_owner(user))
+                  .where(viewable_by_workflow_creator(user)
+                    .or(viewable_by_workflow_owner(user))
                   )
               )
           else
@@ -48,14 +48,17 @@ module Concerns
         end
 
         def viewable_by_workflow_creator(user)
-          collection_media_entry_arcs = Arel::Table.new(:collection_media_entry_arcs)
+          collection_media_entry_arcs =
+            Arel::Table.new(:collection_media_entry_arcs)
           collections = Arel::Table.new(:collections)
           workflows = Arel::Table.new(:workflows)
 
           arel_table
             .project(1)
-            .join(collection_media_entry_arcs).on(collection_media_entry_arcs[:media_entry_id].eq(arel_table[:id]))
-            .join(collections).on(collections[:id].eq(collection_media_entry_arcs[:collection_id]))
+            .join(collection_media_entry_arcs)
+            .on(collection_media_entry_arcs[:media_entry_id].eq(arel_table[:id]))
+            .join(collections)
+            .on(collections[:id].eq(collection_media_entry_arcs[:collection_id]))
             .join(workflows).on(workflows[:id].eq(collections[:workflow_id]))
             .where(workflows[:is_active].eq(true)
               .and(workflows[:creator_id].eq(user[:id]))
@@ -64,15 +67,18 @@ module Concerns
         end
 
         def viewable_by_workflow_owner(user)
-          collection_media_entry_arcs = Arel::Table.new(:collection_media_entry_arcs)
+          collection_media_entry_arcs =
+            Arel::Table.new(:collection_media_entry_arcs)
           collections = Arel::Table.new(:collections)
           workflows = Arel::Table.new(:workflows)
           users_workflows = Arel::Table.new(:users_workflows)
 
           arel_table
             .project(1)
-            .join(collection_media_entry_arcs).on(collection_media_entry_arcs[:media_entry_id].eq(arel_table[:id]))
-            .join(collections).on(collections[:id].eq(collection_media_entry_arcs[:collection_id]))
+            .join(collection_media_entry_arcs)
+            .on(collection_media_entry_arcs[:media_entry_id].eq(arel_table[:id]))
+            .join(collections)
+            .on(collections[:id].eq(collection_media_entry_arcs[:collection_id]))
             .join(workflows).on(workflows[:id].eq(collections[:workflow_id]))
             .join(users_workflows).on(users_workflows[:user_id].eq(user[:id]))
             .where(workflows[:is_active].eq(true))
