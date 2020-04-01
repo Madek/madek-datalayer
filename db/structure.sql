Madek/madek-datalayer

commit e90f61a100632ee9d1bb70c1e99e9d9fcef1b64c

db: add 'seeds' dump (seeds without personas data)

db/structure.sql
@@ -1010,14 +1010,34 @@ CREATE TABLE public.app_settings (
 
 
 --
+-- Name: app_settings_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.app_settings_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: app_settings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.app_settings_id_seq OWNED BY public.app_settings.id;
+
+
+--
 -- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.ar_internal_metadata (
     key character varying NOT NULL,
     value character varying,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
 );
 
 
@@ -1139,8 +1159,8 @@ CREATE TABLE public.collections (
 CREATE TABLE public.confidential_links (
     id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     user_id uuid NOT NULL,
-    resource_id uuid,
     resource_type character varying,
+    resource_id uuid,
     token character varying(45) NOT NULL,
     revoked boolean DEFAULT false NOT NULL,
     description text,
@@ -1345,7 +1365,7 @@ CREATE TABLE public.groups (
     type character varying DEFAULT 'Group'::character varying NOT NULL,
     person_id uuid,
     searchable text DEFAULT ''::text NOT NULL,
-    CONSTRAINT check_valid_type CHECK (((type)::text = ANY (ARRAY[('AuthenticationGroup'::character varying)::text, ('InstitutionalGroup'::character varying)::text, ('Group'::character varying)::text])))
+    CONSTRAINT check_valid_type CHECK (((type)::text = ANY ((ARRAY['AuthenticationGroup'::character varying, 'InstitutionalGroup'::character varying, 'Group'::character varying])::text[])))
 );
 
 
@@ -2174,6 +2194,14 @@ ALTER TABLE ONLY public.rdf_classes
 
 ALTER TABLE ONLY public.roles
     ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.schema_migrations
+    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
 
 
 --
@@ -3459,13 +3487,6 @@ CREATE UNIQUE INDEX unique_login_idx ON public.users USING btree (login);
 
 
 --
--- Name: unique_schema_migrations; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE UNIQUE INDEX unique_schema_migrations ON public.schema_migrations USING btree (version);
-
-
---
 -- Name: users_searchable_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -3743,6 +3764,13 @@ CREATE TRIGGER update_updated_at_column_of_api_tokens BEFORE UPDATE ON public.ap
 --
 
 CREATE TRIGGER update_updated_at_column_of_app_settings BEFORE UPDATE ON public.app_settings FOR EACH ROW WHEN ((old.* IS DISTINCT FROM new.*)) EXECUTE PROCEDURE public.update_updated_at_column();
+
+
+--
+-- Name: ar_internal_metadata update_updated_at_column_of_ar_internal_metadata; Type: TRIGGER; Schema: public; Owner: -
+--
+
+CREATE TRIGGER update_updated_at_column_of_ar_internal_metadata BEFORE UPDATE ON public.ar_internal_metadata FOR EACH ROW WHEN ((old.* IS DISTINCT FROM new.*)) EXECUTE PROCEDURE public.update_updated_at_column();
 
 
 --
@@ -4332,6 +4360,14 @@ ALTER TABLE ONLY public.confidential_links
 
 
 --
+-- Name: collection_user_permissions fk_rails_8f830fb7e7; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.collection_user_permissions
+    ADD CONSTRAINT fk_rails_8f830fb7e7 FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
+--
 -- Name: roles fk_rails_973fbfab62; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -4409,6 +4445,14 @@ ALTER TABLE ONLY public.api_tokens
 
 ALTER TABLE ONLY public.keywords
     ADD CONSTRAINT fk_rails_f3e1612c9e FOREIGN KEY (meta_key_id) REFERENCES public.meta_keys(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: filter_set_user_permissions fk_rails_fe38b294ce; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.filter_set_user_permissions
+    ADD CONSTRAINT fk_rails_fe38b294ce FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
 
 
 --

bin/rerun_db_migrations
@@ -1,0 +1,22 @@
+#!/usr/bin/env bash
+set -eux
+
+SQUASHED_MIGRATIONS_VERSION=340
+
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+
+# seeds (only seeds, like the core vocab)
+bundle exec rails db:pg:terminate_connections db:drop db:create db:migrate VERSION=$SQUASHED_MIGRATIONS_VERSION
+# bundle exec rails db:pg:truncate_tables db:pg:data:restore FILE=db/seeds.$SQUASHED_MIGRATIONS_VERSION.pgbin
+bundle exec rails db:migrate
+bundle exec rails db:seed
+rm -f db/seeds.pgbin
+bundle exec rails db:pg:structure_and_data:dump FILE=db/seeds.pgbin
+
+# personas (seeds and example data)
+bundle exec rails db:pg:terminate_connections db:drop db:create db:migrate VERSION=$SQUASHED_MIGRATIONS_VERSION
+bundle exec rails db:pg:truncate_tables db:pg:data:restore FILE=db/personas.$SQUASHED_MIGRATIONS_VERSION.pgbin
+bundle exec rails db:migrate
+bundle exec rails db:seed
+rm -f db/personas.pgbin
+bundle exec rails db:pg:structure_and_data:dump FILE=db/personas.pgbin

bin/rerun_personas_migrations
@@ -1,8 +1,3 @@
-#!/usr/bin/env bash
-set -eux
-bundle exec rails db:pg:terminate_connections db:drop db:create db:migrate VERSION=340
-bundle exec rails db:pg:truncate_tables db:pg:data:restore FILE=db/personas.340.pgbin
-bundle exec rails db:migrate
-bundle exec rails db:seed
-rm -f db/personas.pgbin
-bundle exec rails db:pg:structure_and_data:dump FILE=db/personas.pgbin
+#!/bin/sh
+echo 'this script was renamed, this is just an alias to not break any workflows'
+bin/rerun_db_migrations 

bin/restore_personas_data
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 set -eux
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+
 bundle exec rake db:pg:truncate_tables db:pg:data:restore FILE=db/personas.pgbin

bin/restore_seeds_data
@@ -1,0 +1,5 @@
+#!/usr/bin/env bash
+set -eux
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+
+bundle exec rake db:pg:truncate_tables db:pg:data:restore FILE=db/seeds.pgbin

commit 0a9c0e4b7fd06d6d9489477c8937e94587f4ca42

db: fix personas dump
was broken in https://github.com/Madek/madek-datalayer/commit/e90f61a100632ee9d1bb70c1e99e9d9fcef1b64c

db/structure.sql
@@ -1010,34 +1010,14 @@ CREATE TABLE public.app_settings (
 
 
 --
--- Name: app_settings_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.app_settings_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: app_settings_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.app_settings_id_seq OWNED BY public.app_settings.id;
-
-
---
 -- Name: ar_internal_metadata; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.ar_internal_metadata (
     key character varying NOT NULL,
     value character varying,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
 );
 
 
@@ -1159,8 +1139,8 @@ CREATE TABLE public.collections (
 CREATE TABLE public.confidential_links (
     id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
     user_id uuid NOT NULL,
-    resource_type character varying,
     resource_id uuid,
+    resource_type character varying,
     token character varying(45) NOT NULL,
     revoked boolean DEFAULT false NOT NULL,
     description text,
@@ -1365,7 +1345,7 @@ CREATE TABLE public.groups (
     type character varying DEFAULT 'Group'::character varying NOT NULL,
     person_id uuid,
     searchable text DEFAULT ''::text NOT NULL,
-    CONSTRAINT check_valid_type CHECK (((type)::text = ANY ((ARRAY['AuthenticationGroup'::character varying, 'InstitutionalGroup'::character varying, 'Group'::character varying])::text[])))
+    CONSTRAINT check_valid_type CHECK (((type)::text = ANY (ARRAY[('AuthenticationGroup'::character varying)::text, ('InstitutionalGroup'::character varying)::text, ('Group'::character varying)::text])))
 );
 
 
@@ -2194,14 +2174,6 @@ ALTER TABLE ONLY public.rdf_classes
 
 ALTER TABLE ONLY public.roles
     ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
-
-
---
--- Name: schema_migrations schema_migrations_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.schema_migrations
-    ADD CONSTRAINT schema_migrations_pkey PRIMARY KEY (version);
 
 
 --
@@ -3487,6 +3459,13 @@ CREATE UNIQUE INDEX unique_login_idx ON public.users USING btree (login);
 
 
 --
+-- Name: unique_schema_migrations; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX unique_schema_migrations ON public.schema_migrations USING btree (version);
+
+
+--
 -- Name: users_searchable_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -3764,13 +3743,6 @@ CREATE TRIGGER update_updated_at_column_of_api_tokens BEFORE UPDATE ON public.ap
 --
 
 CREATE TRIGGER update_updated_at_column_of_app_settings BEFORE UPDATE ON public.app_settings FOR EACH ROW WHEN ((old.* IS DISTINCT FROM new.*)) EXECUTE PROCEDURE public.update_updated_at_column();
-
-
---
--- Name: ar_internal_metadata update_updated_at_column_of_ar_internal_metadata; Type: TRIGGER; Schema: public; Owner: -
---
-
-CREATE TRIGGER update_updated_at_column_of_ar_internal_metadata BEFORE UPDATE ON public.ar_internal_metadata FOR EACH ROW WHEN ((old.* IS DISTINCT FROM new.*)) EXECUTE PROCEDURE public.update_updated_at_column();
 
 
 --
@@ -4360,14 +4332,6 @@ ALTER TABLE ONLY public.confidential_links
 
 
 --
--- Name: collection_user_permissions fk_rails_8f830fb7e7; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.collection_user_permissions
-    ADD CONSTRAINT fk_rails_8f830fb7e7 FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
-
-
---
 -- Name: roles fk_rails_973fbfab62; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -4445,14 +4409,6 @@ ALTER TABLE ONLY public.api_tokens
 
 ALTER TABLE ONLY public.keywords
     ADD CONSTRAINT fk_rails_f3e1612c9e FOREIGN KEY (meta_key_id) REFERENCES public.meta_keys(id) ON UPDATE CASCADE ON DELETE CASCADE;
-
-
---
--- Name: filter_set_user_permissions fk_rails_fe38b294ce; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.filter_set_user_permissions
-    ADD CONSTRAINT fk_rails_fe38b294ce FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
 
 
 --