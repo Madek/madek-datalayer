8b246bb70b8d386c72b23df70f1d34a211e7917a
Max Albrecht <max.albrecht@zhdk.ch>
2022-01-17 15:31:33 +0100

    chore: fix migration

--- db/migrate/318_remove_updated_at_from_edit_sessions.rb
+++ db/migrate/318_remove_updated_at_from_edit_sessions.rb
@@ -1,7 +1,7 @@
 class RemoveUpdatedAtFromEditSessions < ActiveRecord::Migration[4.2]
   def change
     execute <<-SQL.strip_heredoc
-      DROP TRIGGER update_updated_at_column_of_edit_sessions ON edit_sessions;
+      DROP TRIGGER IF EXISTS update_updated_at_column_of_edit_sessions ON edit_sessions;
       ALTER TABLE edit_sessions DROP COLUMN updated_at CASCADE;
     SQL
   end
