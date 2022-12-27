139de005d35c4a80653c93db244b8e7b7c00cd9f
Matus Kmit <matuskmit1@gmail.com>
2022-12-27 18:13:15 +0100

    fix: check secrets entropy (rails 6.0)

--- initializers/check_secrets_entropy.rb
+++ initializers/check_secrets_entropy.rb
@@ -2,17 +2,11 @@ unless Rails.env.development? \
   || Rails.env.test? \
   || ENV['DISABLE_SECRETS_STRENGTH_CHECK'].present?
 
-  unless StrongPassword::StrengthChecker.new(
-    Rails.application.secrets.secret_key_base.to_s
-  ).calculate_entropy > 30
-
+  unless StrongPassword::StrengthChecker.new.calculate_entropy(Rails.application.secrets.secret_key_base.to_s) > 30
     raise 'The entropy of `secret_key_base` is too low.'
   end
 
-  unless StrongPassword::StrengthChecker.new(
-    Settings.madek_master_secret.to_s
-  ).calculate_entropy > 30
-
+  unless StrongPassword::StrengthChecker.new.calculate_entropy(Settings.madek_master_secret.to_s) > 30
    raise 'The entropy of `madek_master_secret` is too low.'
   end
 
