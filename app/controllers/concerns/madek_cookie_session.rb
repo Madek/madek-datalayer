c054b43ee30d70652fa3ae5548cc06c010a58b2f
Matus Kmit <matuskmit1@gmail.com>
2023-08-04 13:35:18 +0200

    notify when session expiring soon

--- app/controllers/concerns/madek_cookie_session.rb
+++ app/controllers/concerns/madek_cookie_session.rb
@@ -6,6 +6,18 @@ module Concerns
 
     COOKIE_NAME = Madek::Constants::MADEK_SESSION_COOKIE_NAME
 
+    def notify_if_session_expiring_soon
+      if session_cookie and s = get_valid_session(session_cookie)
+        expiry_time = s.created_at + s.auth_system.session_max_lifetime_hours.hours
+
+        if DateTime.now + 10.minutes > expiry_time
+          flash[:error] = I18n.t(:session_expiring_soon, minutes: 10)
+        elsif DateTime.now + 30.minutes > expiry_time
+          flash[:warning] = I18n.t(:session_expiring_soon, minutes: 30)
+        end
+      end
+    end
+
     def set_madek_session(user, auth_system, remember = false)
       @session = UserSession.create!(
         user: user, 
@@ -28,18 +40,19 @@ module Concerns
       cookies.delete COOKIE_NAME
     end
 
+    def get_valid_session(session_cookie)
+      UserSession.joins(<<-SQL.strip_heredoc)
+        INNER JOIN auth_systems ON user_sessions.auth_system_id = auth_systems.id
+        AND (user_sessions.created_at 
+          + auth_systems.session_max_lifetime_hours * interval '1 hour') > now()
+      SQL
+      .find_by(["token_hash = ?", 
+                Base64.strict_encode64(Digest::SHA256.digest(session_cookie))])
+    end
+
     def validate_services_session_cookie_and_get_user
       begin
-        @session = 
-         UserSession.joins(
-         <<-SQL.strip_heredoc
-           INNER JOIN auth_systems ON user_sessions.auth_system_id = auth_systems.id
-           AND (user_sessions.created_at 
-            + auth_systems.session_max_lifetime_hours * interval '1 hour') > now()
-         SQL
-           ).find_by(
-             ["token_hash = ?", 
-              Base64.strict_encode64(Digest::SHA256.digest(session_cookie))])
+        @session = get_valid_session(session_cookie!)
         @session || raise(StandardError, 'No valid user_session found')
         @session.user
       rescue Exception => e
@@ -50,8 +63,11 @@ module Concerns
     end
 
     def session_cookie
-      cookies[COOKIE_NAME] || \
-        raise(StandardError, 'Session cookie not found.')
+      cookies[COOKIE_NAME]
+    end
+
+    def session_cookie!
+      session_cookie || raise(StandardError, 'Session cookie not found.')
     end
 
   end
