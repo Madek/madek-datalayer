c991ead2d6590a0670d6c1357dad2393b5dd6f58
uvb <urs.vanbinsbergen@zhdk.ch>
2024-10-28 17:10:49 +0100

    fix: find resource by custom_url (irt rails 7 update)

--- app/models/concerns/media_resources/custom_urls.rb
+++ app/models/concerns/media_resources/custom_urls.rb
@@ -10,12 +10,18 @@ module MediaResources
         custom_urls.find_by(is_primary: true).try(:to_param) or super
       end
 
+      # Use this instead of `find_by_id` to lookup the resource not only by id, but also by custom url id (returns nil when not found)
       def self.find_by_id_or_custom_url_id(arg)
         if arg.is_a?(String) and not arg.match UUIDTools::UUID_REGEXP
           joins(:custom_urls).where(custom_urls: { id: arg }).first
         else
           where(id: arg).first
         end
+      end
+
+      # Use this instead of `find` to lookup the resource not only by id, but also by custom url id (throws ActiveRecord::RecordNotFound when not found)
+      def self.find_by_id_or_custom_url_id_or_raise(arg)
+        find_by_id_or_custom_url_id(arg) or raise ActiveRecord::RecordNotFound
       end
     end
   end
--- app/models/media_entry.rb
+++ app/models/media_entry.rb
@@ -1,21 +1,4 @@
 class MediaEntry < ApplicationRecord
-  ################################################################################
-  # NOTE: The standard `find` and `find_by_id` methods are extended/overridden in
-  # app/models/concerns/media_resources/custom_urls in order to accomodate
-  # custom_ids. One can thus search for a particular resource using either its
-  # uuid or custom_id.
-  # There are two possible use cases:
-  # 1. raise if resource is not found => use `find`
-  # 2. return nil if resource is not found => use `find_by_id`
-  #
-  # `find_by(...)` or `find_by!(...)` are DISABLED. If you want to further
-  # narrow down the scope when searching with a custom_id then do it this way:
-  # Ex. `MediaEntry
-  #        .joins(:custom_urls)
-  #        .where(custom_urls: { is_primary: true })
-  #        .find('custom_id')
-  ################################################################################
-
   VIEW_PERMISSION_NAME = :get_metadata_and_previews
   EDIT_PERMISSION_NAME = :edit_metadata
   MANAGE_PERMISSION_NAME = :edit_permissions
