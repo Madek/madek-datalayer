a8a5f3beb02c2fbf0d4cb6eaff180a4bb559380a
Matus Kmit <matuskmit1@gmail.com>
2024-11-04 14:36:10 +0100

    fix: various codetags

--- app/models/concerns/collections/siblings.rb
+++ app/models/concerns/collections/siblings.rb
@@ -4,8 +4,10 @@ module Collections
     def sibling_collections
       Collection
         .joins(:collection_collection_arcs_as_child)
-        .where(collection_collection_arcs:
-                  { parent_id: parent_collections.select(:id) })
+        .where(collection_collection_arcs: {
+          parent_id: parent_collections.select(:id).reorder(nil)
+        })
+        .where.not(collections: { id: self.id })
     end
 
   end
