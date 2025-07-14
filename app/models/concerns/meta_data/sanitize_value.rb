cbb8d83d91ba39c2f236efc7b4d0251e933bcf42
uvb <urs.vanbinsbergen@zhdk.ch>
2025-07-14 17:18:20 +0200

    WIP - extended sanitizer for people with roles (not finished yet)

--- app/models/concerns/meta_data/sanitize_value.rb
+++ app/models/concerns/meta_data/sanitize_value.rb
@@ -3,7 +3,11 @@ module MetaData
 
     def reset_with_sanitized_value!(resources, type, acting_user = nil)
       with_sanitized(resources, acting_user) do |resources|
-        reset_resources!(resources, type, acting_user)
+        if meta_key.can_have_roles?
+          reset_resources_people_roles!(resources, acting_user)
+        else
+          reset_resources!(resources, type, acting_user)
+        end
       end
     end
 
@@ -41,6 +45,28 @@ module MetaData
       save!
     end
 
+    def reset_resources_people_roles!(person_role_tuples, acting_user)
+      # TODO: index
+      tuple_set = person_role_tuples.map { |x| [x.first, x.second] }.to_set
+      assignments_to_keep = meta_data_people.select do |mdp|
+        tuple_set.include?([mdp.person_id, mdp.role])
+      end
+
+      assignments_to_remove = meta_data_people - assignments_to_keep
+      new_tuples = person_role_tuples - assignments_to_keep.map { |a| [a.person, a.role] }
+      
+      assignments_to_remove.each(&:destroy)
+      new_tuples.each do |person, role|
+        meta_data_people << MetaDatum::Person.new(
+          person: person,
+          role: role,
+          position: 0, # todo
+          created_by: acting_user
+        )
+      end
+      save!
+    end
+
     def reject_blanks_and_modelify_if_necessary(vals, acting_user)
       vals
         .reject(&:blank?)
@@ -49,14 +75,28 @@ module MetaData
 
     def modelify_if_necessary(val, acting_user)
       if self.value.class < ActiveRecord::Associations::CollectionProxy
-        model = self.value.klass.find_or_build_resource!(val, self)
-        if model.respond_to?(:creator_id)
-          model.creator_id = acting_user.id
+        is_people = (self.value.klass == MetaDatum::Person)
+        assignee_class = is_people ? Person : self.value.klass
+        if meta_key.can_have_roles?
+          person_id = extract_person_id(val)
+          role_id = extract_role_id(val)
+          p = modelify(assignee_class, person_id, acting_user)
+          r = role_id && modelify(Role, role_id, acting_user)
+          [p, r]
+        else
+          modelify(assignee_class, val, acting_user)
         end
-        model
       else
         val
       end
+    end
+
+    def modelify(klass, val, acting_user)
+      model = klass.find_or_build_resource!(val, self)
+      if model.respond_to?(:creator_id)
+        model.creator_id = acting_user.id
+      end
+      model
     end
 
     def extract_from_array_if_necessary(val)
@@ -66,5 +106,20 @@ module MetaData
     def need_to_extract_from_array?
       not self.value.class < ActiveRecord::Associations::CollectionProxy
     end
+
+    def extract_person_id(data)
+      if data.is_a?(ActionController::Parameters)
+        data.fetch('uuid', data)
+      else
+        data
+      end
+    end
+
+    def extract_role_id(data)
+      if data.is_a?(ActionController::Parameters)
+        data['role']
+      end
+    end
+
   end
 end
